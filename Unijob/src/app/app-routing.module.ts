ae4b6a6e3b7ff9ba0e23701ea1e36612d60fa4b0
Fabian <fabian.salas.astete@gmail.com>
2021-09-13 00:25:12 -0300

    Pages inicio y login creadas

--- Unijob/src/app/app-routing.module.ts
+++ Unijob/src/app/app-routing.module.ts
@@ -8,8 +8,16 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./Pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./Pages/login/login.module').then( m => m.LoginPageModule)
   },
 ];
 
