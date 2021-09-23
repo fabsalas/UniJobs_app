9b1a7b522e7e6259e7df8700839432bb987e9776
Fabian <fabian.salas.astete@gmail.com>
2021-09-23 18:53:54 -0300

    Agregamos nueva page "empleo-detalle"
    hacemos un pagina hijo "children" con path a la nueva page creada

---
+++ Unijob/src/app/home/empleo-detalle/empleo-detalle.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleo-detalle',
+  templateUrl: './empleo-detalle.page.html',
+  styleUrls: ['./empleo-detalle.page.scss'],
+})
+export class EmpleoDetallePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
