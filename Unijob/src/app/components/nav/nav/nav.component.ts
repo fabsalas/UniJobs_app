d21c7e53920b8610351e5a34a2f4bbfd6b2d781c
Francisco <franci.arayag@duocuc.cl>
2021-10-22 23:26:49 -0300

    Ubicación y cambios de diseño
    nada realmente importante

---
+++ Unijob/src/app/components/nav/nav/nav.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+})
+export class NavComponent {
+
+  constructor() {}
+    
+
+ 
+  ngOnInit() {}
+
+}
